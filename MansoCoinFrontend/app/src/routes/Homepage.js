b5456ef3bf9046c6ea878905ff7a222579558e74
FrederikBRoth <frederikbroth@gmail.com>
2022-03-11 14:59:37 +0000

    Fixed icon, adjusted h1 and h2 font

--- MansoCoinFrontend/app/src/routes/Homepage.js
+++ MansoCoinFrontend/app/src/routes/Homepage.js
@@ -4,13 +4,25 @@ function Homepage() {
     return (
         <main>
             <div className="home">
+                <div className="homepage-picture">
+
+                </div>
                 <div className="header">
                     <h1 className="title">
-                        Manso Coin - Better than an IOU
+                        Manso Coin
                     </h1>
                 </div>
-                <div>
-                    This is the part where you explain the overall stuff
+                <div className="sub-header">
+                    <h2>A cooler, yet much more elaborate way to handle favours</h2>
+                </div>
+                <div className="icon">
+                    <div className="icon-child"></div>
+                </div>
+                <div className="sidebar">
+
+                </div>
+                <div className="bottom">
+
                 </div>
             </div>
         </main>
