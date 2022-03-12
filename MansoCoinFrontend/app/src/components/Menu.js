844b6ffec543455d0f6a41855a0a2bd8c3e144cf
FrederikBRoth <frederikbroth@gmail.com>
2022-03-12 00:21:32 +0000

    Fixed animation code to play by React rules

--- MansoCoinFrontend/app/src/components/Menu.js
+++ MansoCoinFrontend/app/src/components/Menu.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useHistory } from 'react-router-dom';   
 import '../stylesheets/Menu.css';
 
 function Menu(props) {
 
-
-    const [menuOpen, setMenuOpen] = useState(false)
+    const defaultAnimationRange = {startPoint: 0.5, endPoint: 0.8}
+    const animationRange = useRef(defaultAnimationRange)
     const [animationProgress, setAnimationProgress] = useState(0.0)
     const [scrollPosition, setScrollPosition] = useState(0.0);
-    const startPoint = 0.5
-    const endPoint = 0.8
 
     const handleScroll = () => {
-        console.log(window.innerHeight)
         const position = Math.floor(window.scrollY)/window.innerHeight;
         setScrollPosition(position);
         setAnimationProgress(menuAnimation(position))
@@ -20,11 +17,26 @@ function Menu(props) {
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll, { passive: true });
-
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    //IDEA
+    //Transition the color when scrolling down. That shit might look fucking fire
+
+    function menuToggle(open){
+        //This goes against everything React stands for, but i just can't be assed to fix it. Maybe do Refs, but i don't have the patience to fix it
+        const menuOpenRange = {
+            startPoint: 0.3,
+            endPoint: 0.4
+        }
+        if(open){
+            animationRange.current = menuOpenRange
+        } else {
+            animationRange.current = defaultAnimationRange
+        }
+    }
 
     function range(min, max, input){
         let percent = (input - min) / (max - min)
@@ -32,11 +44,13 @@ function Menu(props) {
     }
 
     function menuAnimation(scrollPos) {
-        if(scrollPos > startPoint && scrollPos < endPoint) {
-            return range(startPoint, endPoint, scrollPos)
-        } else if (scrollPos < startPoint) {
+        if(scrollPos > animationRange.current.startPoint && scrollPos < animationRange.current.endPoint) {  
+            return range(animationRange.current.startPoint, animationRange.current.endPoint, scrollPos)
+        } else if (scrollPos < animationRange.current.startPoint) {
+            menuToggle(false)
             return 0
         } else {
+            menuToggle(true)
             return 1
         }
     }
